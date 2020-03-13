d57f2c72a4b4985be7ffa1d2a2c0ff44e853823c
amami0522 <49674604+amami0522@users.noreply.github.com>
2020-03-14 01:17:54 +0900

    最大公約数(GCD),

---
+++ GCD_LCM.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long gcd(long long x, long long y){
+    return __gcd(x, y);
+}
+long long lcm(long long x, long long y){
+    return x / __gcd(x, y) * y;
+}
+
+int main()
+{
+    long long x,y;
+    cin >> x >> y;
+    cout << "GCD(" << x << ", " << y << ") = " << gcd(x,y) << endl;
+    cout << "LCM(" << x << ", " << y << ") = " << lcm(x,y) << endl;
+    return 0;
+}
+
+/*
+example
+
+gcd(x,y)    最大公約数
+lcm(x,y)    最小公倍数
+
+*/
