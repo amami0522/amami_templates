a94fbf8f328d32e893e06a642e57da71dba262bc
amami0522 <49674604+amami0522@users.noreply.github.com>
2020-04-18 23:35:28 +0900

    GCD, LCM

--- GCD_LCM.cpp
+++ GCD_LCM.cpp
@@ -1,27 +1,8 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-long long gcd(long long x, long long y){
-    return __gcd(x, y);
+long long GCD(long long x, long long y) {
+	if(y == 0) return x;
+	return GCD(y, x % y);
 }
 
-long long lcm(long long x, long long y){
-    return x / __gcd(x, y) * y;
+long long LCM(long long x, long long y) {
+	return x * y / GCD(x, y);
 }
-
-int main()
-{
-    long long x,y;
-    cin >> x >> y;
-    cout << "GCD(" << x << ", " << y << ") = " << gcd(x,y) << endl;
-    cout << "LCM(" << x << ", " << y << ") = " << lcm(x,y) << endl;
-    return 0;
-}
-
-/*
-example
-
-gcd(x,y)    最大公約数
-lcm(x,y)    最小公倍数
-
-*/
