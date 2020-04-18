0c0a8cc584eb4a9db54b9c9e8b06fdd9f33efa2d
amami0522 <49674604+amami0522@users.noreply.github.com>
2020-04-18 22:49:39 +0900

    素因数分解

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 map<long long,long long> prime_factor(long long n) {
     map<long long,long long> ret;
     for(long long i = 2; i * i <= n; i++) {
@@ -12,26 +9,3 @@ map<long long,long long> prime_factor(long long n) {
     if(n != 1) ret[n] = 1;
     return ret;
 }
-
-int main()
-{
-    long long n;
-    cin >> n;
-    map<long long,long long> mp=prime_factor(n);
-    for(auto& e:mp){
-        cout << e.first << "^" << e.second << " ";
-    }
-    cout << endl;
-    return 0;
-}
-
-/*
-sample
-
-input
-140
-
-output
-2^2 5^1 7^1
-
- */
