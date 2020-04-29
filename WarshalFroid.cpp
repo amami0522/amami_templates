4c0d4ea32baeb981f6f93c13b3088281e2b1806b
amami0522 <49674604+amami0522@users.noreply.github.com>
2020-04-29 16:16:55 +0900

    ワーシャルフロイド法

--- WarshalFroid.cpp
+++ WarshalFroid.cpp
@@ -1,7 +1,9 @@
 for(int k=0; k < n; k++) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            chmin(dist[i][j], dist[i][k] + dist[k][j]);
+            if(dist[i][k]!=INF && dist[k][j]!=INF){
+                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
         }
     }
 }
